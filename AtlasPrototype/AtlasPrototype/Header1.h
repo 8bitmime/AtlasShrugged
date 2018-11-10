ff3bede2cc202fed7ad9e2d7c84c08ecd763210d
Rubicj <iancj97@gmail.com>
2018-11-10 15:29:40 -0500

    Added method headers

--- AtlasPrototype/AtlasPrototype/Header1.h
+++ AtlasPrototype/AtlasPrototype/Header1.h
@@ -4,3 +4,5 @@
 void doNothing() { return; }
 int returnSeven() { return 7; }
 int returnEight() { return 8; }
+
+bool atlasLoop(LoopStyle style, std::list<bool(*)()> listOfActions, std::list<std::list<int>> listOfJumpBacks, std::list<std::list<int>> listOfFailuresReq, std::list<int> breakpointIndexes);
