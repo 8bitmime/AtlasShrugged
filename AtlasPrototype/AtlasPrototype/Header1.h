ef7412f15559e936d06673f4cedbca4ad0568120
Rubicj <iancj97@gmail.com>
2018-11-10 14:12:48 -0500

    Added basic tests

--- AtlasPrototype/AtlasPrototype/Header1.h
+++ AtlasPrototype/AtlasPrototype/Header1.h
@@ -2,3 +2,5 @@
 
 #include "pch.h"
 void doNothing() { return; }
+int returnSeven() { return 7; }
+int returnEight() { return 8; }
--- GoogleTestAtlas/GoogleTestAtlas/test.cpp
+++ GoogleTestAtlas/GoogleTestAtlas/test.cpp
@@ -1,6 +1,16 @@
 #include "pch.h"
-
-TEST(TestCaseName, TestName) {
+#include "../../AtlasPrototype/AtlasPrototype/Header1.h"
+TEST(MyFirstTestCase, MyFirstTest) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+  doNothing();
+  EXPECT_EQ(returnSeven(), 7);
+  EXPECT_EQ(returnEight(), 8);
+  EXPECT_NE(returnSeven(), 8);
+}
+
+TEST(MyFirstTestCase, MySecondTest) {
+	EXPECT_EQ(1, 1);
+	doNothing();
+	EXPECT_NE(returnEight(), returnSeven());
 }
