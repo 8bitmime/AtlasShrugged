d5ba98cf00ab76e9fbc8a7ba55f6f230866f6335
Rubicj <iancj97@gmail.com>
2018-11-12 11:02:44 -0500

    added ActionObject Object

---
+++ AtlasPrototype/AtlasPrototype/ActionObject.cpp
@@ -0,0 +1,52 @@
+#include "pch.h"
+
+class PairInt
+{
+	public:
+		int failNum;
+		int resultJump;
+	PairInt() {
+		//default constructor
+	}
+	PairInt(int fail, int result) {
+		failNum = fail;
+		resultJump = result;
+	}
+	void printValues() {
+		std::cout << "I have failNum " << failNum << " and resultJump " << resultJump << ". ";
+	}
+	void printLessVerbose() {
+		std::cout << "failNum " << failNum << " and resultJump " << resultJump <<", \n";
+	}
+
+};
+
+class ActionObject
+{
+	public:
+		bool(*ptrActionFunction)();
+		std::list<PairInt> resultList;
+		int minSuccess;
+
+	ActionObject() {
+		//default constructor
+	}
+
+	ActionObject(bool(*givenPointer)(), std::list<PairInt> list, int ms) {
+		ptrActionFunction = givenPointer;
+		resultList = list;
+		minSuccess = ms;
+	}
+	void printEachPair() {
+		for (PairInt p : resultList) {
+			p.printLessVerbose();
+		}
+	}
+
+	void printValues() {
+		std::cout << "I have pointer " << ptrActionFunction << ", \n";
+		printEachPair();
+		std::cout << "and minSuccess " << minSuccess << ".\n";
+
+	}
+};
