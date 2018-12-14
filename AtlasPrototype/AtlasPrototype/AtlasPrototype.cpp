e1f787dfd032e17aa1ecd0741374a11b5e9776bd
Rubicj <iancj97@gmail.com>
2018-12-14 13:55:39 -0500

    added timing code

--- AtlasPrototype/AtlasPrototype/AtlasPrototype.cpp
+++ AtlasPrototype/AtlasPrototype/AtlasPrototype.cpp
@@ -6,6 +6,8 @@
 #include <iterator>
 
 #define VERBOSE_FLAG false
+#define MAX_COUNT 500
+#define NUM_ITERATIONS 1000
 
 bool atlasLoop(std::list<ActionObject> listActions, std::list<int> breakpointIndexesIn) {
 	return atlasLoop(Standard, listActions, breakpointIndexesIn);
@@ -144,9 +146,136 @@ std::list<ActionObject> deSugar(ool aoArr[], int sizeOfArray) {
 	return listOfAO;
 }
 
+bool countReturnTrue1() {
+	for (int k = 0; k < MAX_COUNT; k++) {
+		int arrResults[MAX_COUNT + 10];
+		for (int i = 0; i < MAX_COUNT; i++) {
+			arrResults[i] = i;
+		}
+	}
+	return true;
+}
+bool countReturnTrue2() {
+	return countReturnTrue1();
+}
+bool countReturnTrue3() {
+	return countReturnTrue1();
+}
+bool countReturnTrue4() {
+	return countReturnTrue1();
+}
+bool countReturnTrue5() {
+	return countReturnTrue1();
+}
+bool countReturnTrue6() {
+	return countReturnTrue1();
+}
+bool countReturnTrue7() {
+	return countReturnTrue1();
+}
+bool countReturnTrue8() {
+	return countReturnTrue1();
+}
+bool countReturnTrue9() {
+	return countReturnTrue1();
+}
+bool countReturnTrue10() {
+	return countReturnTrue1();
+}
+
 int main()
 {
 
+	std::cout << "Starting natural loop:\n";
+	
+	float durations[NUM_ITERATIONS];
+	for (int i = 0; i < NUM_ITERATIONS; i++) {
+		const clock_t begin_time = clock();
+		for (int k = 0; k < 10; k++) {
+			countReturnTrue1();
+		}
+		const clock_t end_time = clock();
+		float duration = float(end_time - begin_time) / CLOCKS_PER_SEC;
+		durations[i] = duration;
+		std::cout << duration << "\n";
+	}
+	std::cout << "done";
+	float total = 0;
+	for (int i = 0; i < NUM_ITERATIONS; i++) {
+		total += durations[i];
+	}
+	float avgnat = (total / NUM_ITERATIONS);
+	std::cout << "\n Avg natural: " << (total / NUM_ITERATIONS) << "\n";
+	float deviations[NUM_ITERATIONS];
+	for (int i = 0; i < NUM_ITERATIONS; i++) {
+		deviations[i] = ((durations[i] - avgnat) * (durations[i] - avgnat));
+	}
+	float totaldevnat = 0;
+	for (int i = 0; i < NUM_ITERATIONS; i++) {
+		totaldevnat = totaldevnat + deviations[i];
+	}
+	float stddevnat = sqrt(totaldevnat);
+	std::cout << "Std dev of natural:" << sqrt(totaldevnat) << ". \n";
+
+	std::cout << "Starting our loop:\n";
+
+	bool(*pointerToCountReturnTrue1)();
+	pointerToCountReturnTrue1 = &countReturnTrue1;
+	bool(*pointerToCountReturnTrue2)();
+	pointerToCountReturnTrue2 = &countReturnTrue2;
+	bool(*pointerToCountReturnTrue3)();
+	pointerToCountReturnTrue3 = &countReturnTrue3;
+	bool(*pointerToCountReturnTrue4)();
+	pointerToCountReturnTrue4 = &countReturnTrue4;
+	bool(*pointerToCountReturnTrue5)();
+	pointerToCountReturnTrue5 = &countReturnTrue5;
+	bool(*pointerToCountReturnTrue6)();
+	pointerToCountReturnTrue6 = &countReturnTrue6;
+	bool(*pointerToCountReturnTrue7)();
+	pointerToCountReturnTrue7 = &countReturnTrue7;
+	bool(*pointerToCountReturnTrue8)();
+	pointerToCountReturnTrue8 = &countReturnTrue8;
+	bool(*pointerToCountReturnTrue9)();
+	pointerToCountReturnTrue9 = &countReturnTrue9;
+	bool(*pointerToCountReturnTrue10)();
+	pointerToCountReturnTrue10 = &countReturnTrue10;
+	ActionObject objCntReturn1(countReturnTrue1);
+	ActionObject objCntReturn2(countReturnTrue2);
+	ActionObject objCntReturn3(countReturnTrue3);
+	ActionObject objCntReturn4(countReturnTrue4);
+	ActionObject objCntReturn5(countReturnTrue5);
+	ActionObject objCntReturn6(countReturnTrue6);
+	ActionObject objCntReturn7(countReturnTrue7);
+	ActionObject objCntReturn8(countReturnTrue8);
+	ActionObject objCntReturn9(countReturnTrue9);
+	ActionObject objCntReturn10(countReturnTrue10);
+
+	float durations2[NUM_ITERATIONS];
+	for (int i = 0; i < NUM_ITERATIONS; i++) {
+		const clock_t begin_time = clock();
+		atlasLoop(std::list<ActionObject>{objCntReturn1, objCntReturn2, objCntReturn3, objCntReturn4, objCntReturn5, objCntReturn6, objCntReturn7, objCntReturn8, objCntReturn9, objCntReturn10});
+		const clock_t end_time = clock();
+		float duration = float(end_time - begin_time) / CLOCKS_PER_SEC;
+		durations2[i] = duration;
+		std::cout << duration << "\n";
+	}
+	std::cout << "done";
+	total = 0;
+	for (int i = 0; i < NUM_ITERATIONS; i++) {
+		total += durations2[i];
+	}
+	std::cout << "\n Avg our: " << (total / NUM_ITERATIONS) << "\n";
+	float deviations2[NUM_ITERATIONS];
+	for (int i = 0; i < NUM_ITERATIONS; i++) {
+		deviations2[i] = ((durations2[i] - (total / NUM_ITERATIONS)) * (durations2[i] - (total / NUM_ITERATIONS)));
+	}
+	float totaldev = 0;
+	for (int i = 0; i < NUM_ITERATIONS; i++) {
+		totaldev = totaldev + deviations2[i];
+	}
+	std::cout << "Std dev of natural:" << sqrt(totaldev) << ". \n";
+
+	std::cout << "Conclusion: \n Avg nat: " << avgnat << "\n Avg our: " << (total / NUM_ITERATIONS) << "\n stddev nat: " << stddevnat << "\n stddev our: " << sqrt(totaldev);
 	/*
 	ActionObject myObject1;
 	bool(*pointerToReturnTrue)();
